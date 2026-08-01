1f732975eac405416cf6f6d0fdc528eec3b5ea4c
kalilpock <kalilpock54@gmail.com>
2026-08-01 19:49:21 -0300

    feat: configura servidor Express com CORS e integração ao Supabase

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,40 @@
-const express = require("express");
-app = express();
-const porta = 8080;
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
 
-app.use(express.json())
+const app = express();
 
-app.listen(porta, () =>{
-    console.log("Servidor ativo na rede")
+app.use(cors());
+app.use(express.json());
+
+//config supabase
+const supaBaseURL = process.env.SUPABASE_URL;
+const supabaseKEY = process.env.SUPABASE_KEY;
+const supabse = createClient(supaBaseURL,supabaseKEY);
+
+//config de teste
+app.get('/api/status',(req,res)=>{
+    res.json({
+        status:'online',
+        mensagem:"Servidor ativo na rede"
+    })
+})
+
+//rota form login
+app.post('/api/login',  async (req,res)=>{
+
+})
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () =>{
+  console.log(`\n--- SISTEMA ISÓTOPOS ---`);
+  console.log(`[Backend] Rodando na porta: ${PORT}`);
+  
+  if (supabaseUrl && supabaseKey) {
+    console.log(`[Database] Supabase inicializado com sucesso!`);
+  } else {
+    console.log(`[Database] ERRO: Chaves não encontradas.`);
+  }
 })
